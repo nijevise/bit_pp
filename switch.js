42824177b1e026e76faf351549f8836b6f9b20e0
BitStudent <student@bit.rs>
2019-02-12 17:28:56 +0100

    add switch exercises

---
+++ switch.js
@@ -0,0 +1,48 @@
+var input = 17;
+var rezultat;
+switch (input) {
+    case 1:
+        console.log("Ponedeljak");
+        break;
+    case 2:
+        console.log("Utorak");
+        break;
+    case 3:
+        console.log("Sreda");
+        break;
+    case 4:
+        console.log("Cetvrtak");
+        break;
+    case 5:
+        console.log("Petak");
+        break;
+    case 6:
+        console.log("Subota");
+        break;
+    case 7:
+        console.log("Nedelja");
+        break;
+    default:
+        console.log("Unesite broj 1 do 7");
+}
+
+//zadatak 2
+
+var input = 1;
+
+switch (input) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log("Weekdays");
+        break;
+    case 6:
+    case 7:
+        console.log("Weekend");
+
+
+
+
+} 
