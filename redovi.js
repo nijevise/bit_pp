c0e6454e40edc61f9472b92256e03804821ae7d4
BitStudent <student@bit.rs>
2019-02-13 16:57:47 +0100

    Redovi

---
+++ redovi.js
@@ -0,0 +1,11 @@
+<skript>
+    var animals=["Cat", "Dog", "Llama"];
+    var rezultat=[];
+    rezultat=animals.unshift("Majmun");
+    console.log(rezultat);
+   console.log(animals);
+        </skript>
+
+
+var m = ["januar", "februar", "mart"];
+console.log(m[0] + "\n", m[1] + "\t", m[2]);
