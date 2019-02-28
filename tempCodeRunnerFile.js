caaa349c66d81aa126db03c27cb7c8495c6b48c2
nijevise <calsoft@gmail.com>
2019-02-28 22:44:20 +0100

    test funkcije2

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,12 +1,1 @@
-var input = [NaN, 23.1, 15, false, -22.5, '', 4, 7, null];
-function countInteger(bigStr){
-    var smallStr = '';
-    var numberInteger;
-    for (var i = 0; i < bigStr.length; i++){
-      smallStr += bigStr[i] ;
-      numberInteger = parseInt(smallStr);
-       
-    } return numberInteger
-};
-var res = countInteger(input);
-console.log(countInteger(res));
+typeof console.log;
