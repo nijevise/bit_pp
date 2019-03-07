9c3aa953a85b88dac9956f608df8b66fcc6a50b9
BIT Student <classroom@bgit.rs>
2019-03-07 10:28:44 +0100

    extra zadaci

--- built-in-objects/tempCodeRunnerFile.js
+++ built-in-objects/tempCodeRunnerFile.js
@@ -1,9 +1,14 @@
-function hideEmail(stringUrl) {
 
-    var num = stringUrl.indexOf("@") - 9;
-    var strArr = stringUrl.split("")
-    strArr.splice(num, 9, "...")
-    newStr = strArr.join("")
-    console.log(newStr);
+var number = [3, 4, 6, 7, 2, 9, 0]
+
+
+for (var i = 0; i < number.length; i++) {
+    var newPosition = Math.round(Math.random() * (number.length - 1))
+
+    var tmp = number[newPosition];
+    number[newPosition] = number[i];
+    number[i] = tmp
 }
-hideEmail("somerandomaddress@example.com");
+
+
+console.log(number)
