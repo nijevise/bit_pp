da29b0692a81af86c3f2dae8b1f7f58fd8722bba
nijevise <calsoft@gmail.com>
2019-03-07 23:16:54 +0100

    festival i shoping

--- festival.js
+++ festival.js
@@ -1,20 +1,117 @@
+// Festival
+
+// In your IDE of choice, create a new JavaScript file named festival.js and make it so that all code written in the file follows strict mode.
+
+// Create an anonymous immediately-invoking function that will hold the main execution of all program calls. Writing a simple command console.log(“Hi”) inside this function and running code should output “Hi“ in the console.
+
+// Create constructor functions with properties representing the following:
+// Genre - name
+// Movie - title, genre (instance of Genre), length
+// Program - date, list of movies (initially empty) and total number of movies 
+// Festival - name, list of programs (initially empty), and number of movies in all programs
+
+// Add method getData to Genre which returns formatted string as first and last letter of genre name
+// "Action" -> AN
+// "Drama" -> DA
+// "Comedy" -> CY
+
+// Add getData method to Movie. It should return a formatted string consisting of the movie title, length and genre acronym.
+// The Shawshank Redemption, 130min, AN
+
+// Add addMovie method to Program. It should receive a Movie and add the movie to the movie list of a given program. 
+
+// Add addProgram method to Festival. It should receive a Program and add the program to the list of the given festival’s programs. 
+
+// Add getData method to Program. It should return a formatted string of all data related to the program. The string should contain date, program length (calculated from length of all movies in a list) and data about movies in a list (see the example below). To display movie data, use Movie’s getData method.
+
+                        // Date, program length from all movies
+                        //      First movie title, length and genre
+                        //      Second movie title, length and genre
+                        //      Third movie title, length and genre
+                        //      Fourth movie name and length and genre
+
+// 
+// Add method getData to Festival which return formatted string like festival name, number of movies in all programs and all programs listed. Use Programs getData method to list all programs. (example output is shown below)
+
+                            // Weekend festival has 4 movie titles
+                            // 	28.10.2017, program duration 368min
+                            // 		Spider-Man: Homecoming, 133min, AN
+                            // 		War for the Planet of the Apes, 140min, DA
+                            // 		The Dark Tower, 95min, WN
+                            // 	29.10.2017, program duration 108min
+                            // 		Deadpool, 108min, CY
+
+// Inside your immediately-invoking function, add createMovie function that receives a movie title, movie length and genre (as a string) as parameters and returns a created Movie.
+
+// Inside your immediately-invoking function, add createProgram function that receives a date and returns a created Program.
+
+// Start creating your movie festival.
+
+// In your main program function, create an instance of the Festival object. 
+
+// Create two instances of Program using createProgram function.
+
+// Create four instances of Movie object using createMovie function. Add all created movies to both programs using the addMovie method.
+
+// Add the created program instances to the created festival instance using festival’s addProgram method.
+
+// Display festival’s data using getData method.
+
+// Hints
+// List is a synonym for array, so when we say a list of movies, it’s actually an array of movie objects
+// Use JS built-in Date()object to create Date object
+// Use \t and \n special strings to format output 
+// Use built-in String methods to transform text from lowercase to uppercase
+// Use Array’s built-in methods to add and remove elements from an array
+
+
 (function () {
     'use strict'
-    function Festival() {
-        this.genre = name;
-        this.movie = {
-            title: "",
-            genre: this.genre,
-            length = 0,
 
+    function Genre(genre) {
+        var acronym;
+        this.name = name;
+        this.getData = function(){
+            acronym = this.name[0] + this.name[name.length-1].toUpperCase();
+            return acronym;
+             
         }
-        this.program = {
-            date: date,
-            listOfMovies: [],
+    }
+    function Movie(title,duration,genre){
+  
+            this.title= "",
+            this.genre= function(genre){
+                return acronym;
+            }
+            this.duration = 0,
+            this.getData = function(){
+                return this.title + ", " + this.duration + ", " + this.acronym
+            }
+        };     
+var firstMovie = new Movie("Once Upon a Time in America","229min","Drama");
+var secondMovie = new Movie("Upside Down","229min","Fantasy");
+var thirdMovie = new Movie("Billy Elliot","110min","Music");
+var forthMovie = new Movie("Breaking the Waves","159min","Drama");
+    
+    // function Program () {
+    //         this.date = date,
+    //         this.listOfMovies= [],
+    //         this.totalNumOfMovies = 0;
+    //         this.addMovie = function(movie){
+    //             return listOfMovies.push(movie)
+    //         }
+    //     }
 
-        }
-        this.festival =  
-}
+
+    // function Festival() {
+    //     this.name = name;
+    //     this.listOfPrograms = [];
+    //     this.numOfMoviesAll = 0;
+    //     this.addProgram = function(program){
+
+    //     }
+           
+    // }
 
 
 
--- shopping.js
+++ shopping.js
@@ -1,57 +1,86 @@
 // /In your IDE of choice, create a new JavaScript file named shopping.js and make it so that all code written in the file follows strict mode.
-
 // Create an anonymous immediately-invoking function that will hold the main execution of all program calls. Writing a simple command console.log(“Hi”) inside this function and running the code should output “Hi“ in the console.
-
-
 // Create constructor functions with properties representing the following “things”:
 // Product - product id (random number of 5 digits generated on every product creation), name, price (with 2 decimal places), expiration date
 // ShoppingBag - a list of products (initially empty; function constructor does not have any input parameters)
-
 // Add getInfo method to Product. It should return a formatted string containing product code (the first and last letter of the name together with the product id), name and price.
 // "Banana" -> BA32784, Banana, 129.31
-
 // Add addProduct method to ShoppingBag. It should receive a Product and add it to the product list. You can add a product to the list only if it has a valid expiration date.  
-
 // Add a method to ShoppingBag that calculates the average product price of products in your product list and prints out this value with the precision of three decimals. 
-
 // Add getMostExpensive method that finds the most expensive product and prints out its info.
-
 // Add calculateTotalPrice method to ShoppingBag that calculates the total price of products in the shopping bag list. 
-
 // Create a constructor function with properties representing the following:
-
 // PaymentCard - account balance (number with 2 decimal places), active or inactive status, valid until date	
-
 // Create checkoutAndBuy function which receives shopping bag and payment card and prints if the purchase is successful or not. Purchase is successful only if the amount on the card is greater or equal to the total price of products in the shopping bag. If there is not enough money, print out the amount that is missing to complete the purchase.
 
 (function () {
     'use strict'
 
-    function Product(productName, productPrice, productExpDate) {
+    function Product(ID,name,price,expDate) {
         this.productId = Math.floor(Math.random() * 90000) + 10000;
-        this.name = productName;
-        this.price = productPrice;
-        this.productExpDate = new Date(productExpDate);
-
+        this.name = name;
+        this.price = price;
+        this.expirationDate = expDate;
         this.getInfo = function () {
-            var productCode = this.productName[0].toUpperCase() + this.productName[productName.length - 1].toUpperCase();
-            return productCode;
+            return this.name[0].toUpperCase()+this.name[name.length-1].toUpperCase()+this.prodactID+', '+this.name+', '+this.price;
         }
-        return this.productId + this.productCode + ', ' + this.productName + ', ' + this.productPrice;
     }
 
     function ShoppingBag() {
         this.listOfProduct = [],
-
-            this.addProduct = function (product) {
+        this.addProduct = function (product) {
                 var date = new Date();
-                if (product.productExpDate > date) {
+                if (product.expirationDate > date) {
                     this.listOfProduct.push(product)
+                    console.log(prodact.name+" is added to bag");
                 } else {
-                    console.log('Product Date has expired!')
+                    console.log("Date has expired")
                 }
-            }
+            };
+    
+    this.averagePrice = function(){
+        var sum = 0;
+        for( var i =0; i < this.listOfProduct.length; i++){
+            sum += this.listOfProduct[i].price
+        }
+        return( sum/this.listOfProduct.length).toFixed(3);
     }
+
+    this.getMostExpensive = function(){
+        var prices = [];
+        var mostExpensive;
+        for(var i = 0; i < this.listOfProduct.length; i++){
+            prices.push(this.listOfProduct[i].price);
+        }
+        mostExpensive = Math.max(...prices);
+        for (var i = 0; i < this.listOfProduct.length; i++){
+            if (this.listOfProduct[i].price == mostExpensive){
+                return "The most expencive product in bag is "+this.listOfProduct[i].name+", price: "+this.listOfProduct[i].price;
+            }
+        }
+    }
+    this.calculateTotalPrice = function(){
+        var totalPrice = 0;
+        for( var i = 0; i < this.listOfProduct.length; i++ ){
+            totalPrice += this.listOfProduct[i].price;
+        }
+        return totalPrice;
+    }
+
+    function PaymentCard(activeBalace,active,date){
+        this.activeBalace = activeBalace;
+        this.active = active;
+        this.validUntilDate = date;
+    }
+    function checkoutAndBay(bag,card){
+        if(card.activeBalace >= card.activeBalace){
+            console.log("Purchase is succesful")
+        }
+        else{
+            console.log("We are missing "+(bag.calculateTotalPrice-card.activeBalace)+" to complete the perchase");
+        }
+    }
+
 
     var banana = new Product('Banana', 2.56, '01.01.2019');
     var avocado = new Product('Avocado', 4.00, "20/01/2019");
@@ -60,9 +89,8 @@
     console.log(banana)
     console.log(avocado)
     console.log(jabuka)
-
-    var card = new ShoppingBag();
-    card.addProduct(banana);
-    card.addProduct(avocado);
+  
 
 }());
+
+
