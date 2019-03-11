7eb4eeef9019f8e9d277e382f1e91c684c0f2e3c
BIT Student <classroom@bgit.rs>
2019-03-11 18:06:01 +0100

    nemanjine stvari

---
+++ Nemanja/airport.js
@@ -0,0 +1,120 @@
+(function () {
+
+
+    function Person(name, surname) {
+        this.name = name;
+        this.surname = surname;
+
+        this.getData = function () {
+            return this.name + ' ' + this.surname;
+        }
+    }
+
+    var mirko = new Person('Mirko', 'Zivanovic');
+    var slavko = new Person('Slavko', 'Jankovic');
+    var milica = new Person('Milica', 'Jankovic');
+    var dragana = new Person('Dragana', 'Zivanovic');
+
+
+
+    function Seat(number, category) {
+        this.number = !number || typeof number === "string"
+            ? Math.floor(Math.random() * 90) + 10
+            : number;
+
+        this.category = !category ? 'e' : category;
+
+        this.getData = function () {
+            return this.number + ', ' + this.category.toUpperCase();
+        }
+    }
+
+    var e1 = new Seat(12, 'b');
+    var b8 = new Seat(12, null);
+    var e34 = new Seat(null, 'b');
+    var b9 = new Seat(13, null)
+
+
+    function Passenger(person, seat) {
+        this.person = person;
+        this.seat = seat;
+
+        this.getData = function () {
+            return this.seat.getData() + ' ' + this.person.getData()
+        }
+    }
+
+    var pass1 = new Passenger(mirko, e1);
+    var pass2 = new Passenger(dragana, e34);
+    var pass3 = new Passenger(slavko, b8);
+    var pass4 = new Passenger(milica, b9);
+
+
+    function Flight(relation, date) {
+        this.relation = relation;
+        this.date = new Date(date).toDateString();
+        this.listPassangers = [];
+
+        this.addPassangers = function (passanger) {
+            this.listPassangers.push(passanger);
+        }
+
+        this.numOfPass = function () {
+            var num = this.listPassangers.length;
+            return num;
+        };
+
+
+        this.getData = function () {
+            var str = ''
+            str += this.date + ', ' + this.relation + '\n';
+            for (var i = 0; i < this.listPassangers.length; i++) {
+                str += this.listPassangers[i].getData() + '\n'
+            }
+            return str;
+        }
+    }
+
+    var bgny = new Flight('Belgrade - NewYork', 'Oct / 12 / 2019');
+    bgny.addPassangers(pass1);
+    bgny.addPassangers(pass2);
+    var bgpr = new Flight('Belgrade - Paris', 'Oct / 15 / 2019')
+    bgpr.addPassangers(pass3);
+    bgpr.addPassangers(pass4);
+
+
+    function Airport() {
+        this.name = 'Nikola Tesla';
+        this.listOfFlights = []
+
+        this.addFlight = function (flight) {
+            this.listOfFlights.push(flight);
+        }
+
+
+        this.getData = function () {
+            var num = 0
+            var str = 'Airport: ';
+            str += this.name + ', total passangers: '
+            for (var i = 0; i < this.listOfFlights.length; i++) {
+                num += this.listOfFlights[i].numOfPass()
+            }
+            str += num + '\n'
+            for (var i = 0; i < this.listOfFlights.length; i++) {
+                str += '   ' + this.listOfFlights[i].date + ', ' + this.listOfFlights[i].relation + '\n';
+                for (var j = 0; j < this.listOfFlights[i].listPassangers.length; j++) {
+                    str += '        ' + this.listOfFlights[i].listPassangers[j].getData() + '\n'
+                }
+            }
+            return str
+        }
+    }
+
+    var nikolaTesla = new Airport()
+    nikolaTesla.addFlight(bgny);
+    nikolaTesla.addFlight(bgpr);
+    console.log(nikolaTesla.getData())
+
+
+})();
+
---
+++ Nemanja/fest.js
@@ -0,0 +1,115 @@
+
+
+(function () {
+
+    function Genre(name) {
+        this.name = name;
+        this.getInfo = function () {
+            name = this.name.split("");
+            var genInfo = [];
+            genInfo.push(name[0]);
+            genInfo.push(name[name.length - 1]);
+            genInfo = genInfo.join("").toUpperCase();
+
+            return genInfo;
+        }
+    }
+
+    var horror = new Genre('horror');
+    var action = new Genre('action');
+    var drama = new Genre('drama');
+
+
+    function Movie(title, duration, genre) {
+        this.title = title;
+        this.duration = duration;
+        this.genreAcr = genre.getInfo();
+
+        this.getData = function () {
+            return this.title + ' ' + this.duration + 'min' + ' ' + this.genreAcr
+        };
+    }
+
+    var firstMov = new Movie('Halloween', 90, horror);
+    var secondMov = new Movie('Ring', 120, horror);
+    var thirdMov = new Movie('Titanic', 210, drama);
+    var forthMov = new Movie('Mr.Riplay', 180, drama);
+    var fifthMov = new Movie('Tetminator', 130, action);
+    var sixthMov = new Movie('Rambo', 95, action);
+
+
+    function Program(date) {
+        this.date = new Date(date).toDateString();
+        this.listOfMovies = [];
+        this.totalNumOfMovies = function () {
+            var num = this.listOfMovies.length
+            return num
+        }
+
+        this.addMovie = function (movie) {
+            this.listOfMovies.push(movie)
+        }
+
+        this.totalDurationPerProgram = function () {
+            var totalProgramDuration = 0;
+            for (var i = 0; i < this.listOfMovies.length; i++) {
+                totalProgramDuration += this.listOfMovies[i].duration;
+            }
+            return totalProgramDuration + 'min';
+        }
+
+        this.addData = function () {
+            var str = '';
+            str = this.date.getFullYear() + '.' + this.date.getMonth() + '.' + this.date.getDate() + ', ' + this.totalDurationPerProgram() + '\n';
+            for (var i = 0; i < this.listOfMovies.length; i++) {
+                str += this.listOfMovies[i].getData() + '\n'
+            }
+            return str
+        }
+    }
+
+    var firstProgram = new Program('Oct / 2 / 2019');
+    var secondProgram = new Program('Oct / 3 / 2019');
+    var thirdProgram = new Program('Oct / 4 / 2019');
+    firstProgram.addMovie(firstMov);
+    firstProgram.addMovie(secondMov);
+    secondProgram.addMovie(thirdMov);
+    secondProgram.addMovie(forthMov);
+    thirdProgram.addMovie(fifthMov);
+    thirdProgram.addMovie(sixthMov);
+
+    function Festival(name) {
+        this.name = name
+        this.programs = [];
+        this.numOfMoviesInPrograms = 0
+
+        this.addProgram = function (program) {
+            this.programs.push(program);
+        }
+
+        this.getData = function () {
+            str = ''
+            var num = 0
+            for (var i = 0; i < this.programs.length; i++) {
+                num += this.programs[i].totalNumOfMovies()
+            }
+            str += this.name + ' has ' + num + ' movie titles \n';
+            for (var i = 0; i < this.programs.length; i++) {
+                str += this.programs[i].date + ', ' + ' program duration: ' + this.programs[i].totalDurationPerProgram() + '\n'
+                for (var j = 0; j < this.programs[i].listOfMovies.length; j++) {
+                    str += '      ' + this.programs[i].listOfMovies[j].getData() + '\n';
+                }
+            }
+            return str
+        }
+    }
+
+    var fest = new Festival('Fest');
+    var kan = new Festival('Kan');
+    fest.addProgram(firstProgram);
+    fest.addProgram(secondProgram);
+    kan.addProgram(thirdProgram);
+    console.log(fest.getData())
+
+
+})();
