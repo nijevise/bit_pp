901b9d1b878f059db5416cac67cd118a0a37f0ea
BIT Student <classroom@bgit.rs>
2019-03-22 15:34:36 +0100

    fetch api

--- fetchApi/script.js
+++ fetchApi/script.js
@@ -1,8 +1,8 @@
-function createNode(element){
+function createNode(element) {
     return document.createElement(element)
 }
 
-function append(parent,el){
+function append(parent, el) {
     return parent.appendChild(el);
 }
 
@@ -10,31 +10,31 @@ const ul = document.getElementById("authors");
 const url = 'https://randomuser.me/api/?results=10';
 
 fetch(url)
-.then((resp) => resp.json())
-.than(function(data){
-    let authors = data.results;
+    .then((resp) => resp.json())
+    .then(function (data) {
+        let authors = data.results;
 
-    return authors.map(function(authors){
+        return authors.map(function (author) {
 
-        let li = createNode('li'),
-        img = createNode('img'),
-        span = createNode('span');
+            let li = createNode('li'),
+                img = createNode('img'),
+                span = createNode('span');
 
-        img.src=author.picture.medium;
-        
-        span.innerHTML=`${author.name.first} 
+            img.src = author.picture.medium;
+
+            span.innerHTML = `${author.name.first} 
         ${author.name.last}`;
 
-        append(li,img);
-        apppend(li,span);
-        append(ul,li);
+            append(li, img);
+            append(li, span);
+            append(ul, li);
 
 
 
+        })
     })
-})
 
-.catch(function(error){
+    .catch(function (error) {
 
-    console.log(JSON.stringify(error));
-})
+        console.log(JSON.stringify(error));
+    })
