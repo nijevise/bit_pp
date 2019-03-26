b2bf60437a745eb35d97781f302a5b6e728cedab
nijevise <calsoft@gmail.com>
2019-03-26 23:10:23 +0100

    FrontEnd

--- client-server FrontEnd/server/server.js
+++ client-server FrontEnd/server/server.js
@@ -6,7 +6,7 @@ const post = require('./api.js')
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
-    res.setHeader('Content-type', 'application/json')
+    res.setHeader('Content-Type', 'text/plain')
     res.end(JSON.stringify(post))
 })
 
