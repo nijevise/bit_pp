3f3d474a59a6f07e04187221884998e66badae65
nijevise <calsoft@gmail.com>
2019-03-27 23:13:10 +0100

    sitnica

--- client-server FrontEnd/server/server.js
+++ client-server FrontEnd/server/server.js
@@ -5,7 +5,7 @@ const post = require('./api.js')
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000')
     res.setHeader('Content-Type', 'text/plain')
     res.end(JSON.stringify(post))
    
